6cbe5cc9921a3671129106d8c0a690b50c63fa42
Kevin Kee <kevinkee9@gmail.com>
2020-10-29 08:33:39 +0900

    Test four sets of data against implementations in loop

--- test_discount.py
+++ test_discount.py
@@ -5,9 +5,15 @@ from discount import apply_percentage_discount, apply_flat_discount
 class TestDiscounts(unittest.TestCase):
 
     def test_apply_percentage_discount_return_cost_less_discount_of_cost(self):
-        discounted_price = apply_percentage_discount(230, 5)
-        self.assertEqual(219, discounted_price)
+        test_cases = [
+            (230, 5, 219), (937, 10, 844), (767, 23, 591), (1630, 77, 375)]
+        for cost, discount, expected in test_cases:
+            discounted_price = apply_percentage_discount(cost, discount)
+            self.assertEqual(expected, discounted_price)
 
     def test_apply_flat_discount_returns_cost_less_flat_discount(self):
-        discounted_price = apply_flat_discount(10, 5)
-        self.assertEqual(5, discounted_price)
+        test_cases = [
+            (230, 5, 225), (937, 10, 927), (767, 23, 744), (1630, 77, 1553)]
+        for cost, discount, expected in test_cases:
+            discounted_price = apply_flat_discount(cost, discount)
+            self.assertEqual(expected, discounted_price)
